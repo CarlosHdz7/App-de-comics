18acc8964ac1b34bfe52dbce31a67361daca44a8
CarlosHdz7 <carloshdz.udb@gmail.com>
2019-02-21 18:50:51 -0600

    ya carga la información individual correctamente

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HeroesService } from '../../services/heroes.service';
+import { Router } from '@angular/router';
+
 
 
 
@@ -12,11 +14,14 @@ export class HeroeComponent {
 
   heroe: any = {};
 
-  constructor(private heroesService: HeroesService, private activatedRoute: ActivatedRoute) {
+  constructor(private heroesService: HeroesService, private activatedRoute: ActivatedRoute, private router: Router) {
 
     this.activatedRoute.params.subscribe( params => {
         this.heroe = this.heroesService.getHeroe( params['id'] );
     });
+  }
 
+  getHeroes() {
+    this.router.navigate( ['/heroes'] );
   }
 }
